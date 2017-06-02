665f64460933b141479aa0d6a03dc3de409fb3b4
Yevgen Matusevych <yevgen@su1.ais.sandbox>
2017-06-02 16:37:51 -0400

    joined english and dutch, fixed get_connections

--- networks.py
+++ networks.py
@@ -3,141 +3,104 @@ import json
 import networkx as nx
 from networkx.readwrite import json_graph
 from collections import Counter
+import re
 
-# Plotting functions.
-# import plotly
-# from plotly.graph_objs import *
-# def make_annotations(pos, labels, font_size=14, font_color='rgb(25,25,25)'):
-#     L = len(pos)
-#     if len(labels) != L:
-#         raise ValueError('The lists pos and text must have the same len')
-#     annotations = Annotations()
-#     for k in range(L):
-#         annotations.append(
-#             Annotation(
-#                 text=labels[k],
-#                 x=pos[labels[k]][0], y=pos[labels[k]][1] + 0.01,
-#                 xref='x1', yref='y1',
-#                 font=dict(color=font_color, size=font_size),
-#                 showarrow=False)
-#         )
-#     return annotations
-#
-#
-# def plotGraph(G, labels, pos, fname):
-#
-#     edge_trace = Scatter(
-#         x=[],
-#         y=[],
-#         line=Line(width=0.5,color='#888'),
-#         hoverinfo='none',
-#         mode='lines')
-#
-#     for edge in G.edges():
-#         x0, y0 = pos[edge[0]]
-#         x1, y1 = pos[edge[1]]
-#         edge_trace['x'] += [x0, x1, None]
-#         edge_trace['y'] += [y0, y1, None]
-#
-#     node_trace = Scatter(
-#         x=[],
-#         y=[],
-#         text=[],
-#         mode='markers',
-#         hoverinfo='text',
-#         marker=Marker(
-#             showscale=True,
-#             colorscale='YIGnBu',
-#             reversescale=True,
-#             color=[],
-#             size=10,
-#             colorbar=dict(
-#                 thickness=15,
-#                 title='Node Connections',
-#                 xanchor='left',
-#                 titleside='right'
-#             ),
-#             line=dict(width=2)))
-#
-#     for node in G.nodes():
-#         x, y = pos[node]
-#         node_trace['x'].append(x)
-#         node_trace['y'].append(y)
-#
-#     for node, adjacencies in enumerate(G.adjacency_list()):
-#         node_trace['marker']['color'].append(len(adjacencies))
-#         node_info = '# of connections: '+str(len(adjacencies))
-#         node_trace['text'].append(node_info)
-#
-#     plotly.offline.plot({
-#         "data": Data([edge_trace, node_trace]),
-#         "layout": Layout(
-#                     title=fname,
-#                     titlefont=dict(size=16),
-#                     showlegend=False,
-#                     hovermode='closest',
-#                     margin=dict(b=20,l=5,r=5,t=40),
-#                     xaxis=XAxis(showgrid=False, zeroline=False, showticklabels=False),
-#                     yaxis=YAxis(showgrid=False, zeroline=False, showticklabels=False),
-#                     annotations=make_annotations(pos, labels))
-#     }, filename = fname + '.html')
 
-def get_connections(responses, depth, current_depth):
+def Graphnx():
+    count = 1
+    f = open("./Dutch/associationData.csv")
+    l = f.readlines()
+    l = l[1:]
+    G = nx.DiGraph()
+    for line in l:
+        line = line.strip('\n')
+        line = re.sub(r'"', '',line)
+        #line = line.strip('"')
+        #print(count)
+        count = count+1
+        if True:#count%10 == 0:
+            #print(line)
+            w = line.split(";")[2:]
+            #print(w)
+            #print(line)
+            G.add_nodes_from(w)
+            for i in range(3):
+                if w[i+1] != 'x':
+                    if G.has_edge(w[0], w[i+1]):
+                        G[w[0]][w[i+1]]['weight'] = G[w[0]][w[i+1]]['weight'] + 1
+                    else:
+                        G.add_edge(w[0], w[i+1], weight = 1)
+    #print('EEEENNNNNDDDDDD')
+    print(G.number_of_nodes())
+    print(G.number_of_edges())
+    #nx.draw_networkx(G,pos=nx.spring_layout(G))
+    #plt.show()
+    return G
+
+
+def get_english_digraph():
+    if os.path.isfile("./EAT/EATnew_directed"):
+        with open("./EAT/EATnew_directed") as jc:
+            D = json_graph.node_link_graph(json.load(jc))
+    else:
+        M = nx.read_pajek("./EAT/EATnew.net")
+        D = nx.DiGraph()
+        for u, v, data in M.edges_iter(data=True):
+            w = data['weight'] if 'weight' in data else 1.0
+            if D.has_edge(u, v):
+                D[u][v]['weight'] += w
+            else:
+                D.add_edge(u.lower(), v.lower(), weight=w)
+        with open("./EAT/EATnew_directed", 'w') as outfile:
+            json.dump(json_graph.node_link_data(D), outfile)
+    return D
+
+def get_dutch_digraph():
+    if os.path.isfile("./Dutch/associationData_directed"):
+        with open("./Dutch/associationData_directed") as jc:
+            D = json_graph.node_link_graph(json.load(jc))
+    else:
+        D = Graphnx()
+        with open("./Dutch/associationData_directed", 'w') as outfile:
+            json.dump(json_graph.node_link_data(D), outfile)
+    return D
+
+
+def get_connections(G, responses, depth, current_depth):
     if current_depth > depth:
         return(responses)
     else:
         responses_current_level = dict()
         for word in sorted(responses):
             weight = responses[word]
-            new = {r[0]:r[1]['weight'] for r in G.edge[word.upper()].items()}
+            new = {w:e[2] for e in G.out_edges([word], 'weight') for w in str.split(e[1])}
             total = sum(new.values())
             responses_single_word = {k:v/total*weight for k,v in new.items()}
             responses_current_level = dict(sum((Counter(x) for x in [responses_current_level, responses_single_word]), Counter()))
         current_depth += 1
-        responses_next_level = get_connections(responses_current_level, depth, current_depth)
+        responses_next_level = get_connections(G, responses_current_level, depth, current_depth)
         final = dict(sum((Counter(x) for x in [responses_current_level, responses_next_level]), Counter()))
         return(final)
 
-fn="EATnew"
-
-if os.path.isfile("./EAT/"+fn+"_directed") and os.path.isfile("./EAT/"+fn+"_undirected"):
-    with open("./EAT/"+fn+"_directed") as jc:
-        G = json_graph.node_link_graph(json.load(jc))
-else:
-    M = nx.read_pajek("./EAT/"+fn+".net")
-    D = nx.Graph()
-    for u,v,data in M.edges_iter(data=True):
-        w = data['weight'] if 'weight' in data else 1.0
-        if D.has_edge(u,v):
-            D[u][v]['weight'] += w
-        else:
-            D.add_edge(u, v, weight=w)
-    with open("./EAT/"+fn+"_directed", 'w') as outfile:
-        json.dump(json_graph.node_link_data(D), outfile)
-    G = D.to_undirected()
-    with open("./EAT/"+fn+"_undirected", 'w') as outfile:
-        json.dump(json_graph.node_link_data(G), outfile)
-
-test = ["skirt", "potato"]
-
-for w in test:
-    print("CUE:",w)
-    for depth in range(1,3):
-        print("\tMAX DEPTH:", depth)
-        responses = dict(get_connections({w:1}, depth, current_depth=1))
-        responses[w.upper()] = 0
-        responses[w] = 0
-        for k, v in sorted(responses.items(), key=lambda x: x[1], reverse=True)[:5]:
-            print("\t\t%s\t\t%.3f" % (k, v))
+def test_network(D, test_list, max_depth):
+    for w in test_list:
+        print("CUE:",w)
+        for depth in range(1, max_depth):
+            print("\tMAX DEPTH:", depth)
+            responses = dict(get_connections(D, {w:1}, depth, current_depth=1))
+            responses[w] = 0
+            for k, v in sorted(responses.items(), key=lambda x: x[1], reverse=True)[:5]:
+                print("\t\t%s\t\t%.3f" % (k, v))
 
 
-# Plotting.
-# pos=nx.spring_layout(G)
-# plotGraph(G, list(G.edge.keys()), pos, "eap_plot")
-#
-# part_nat = community.best_partition(G)
-# mod_nat = community.modularity(part_nat,G)
-# values = [part_nat.get(node) for node in G.nodes()]
-# nx.draw_spring(G, cmap=plt.get_cmap('jet'), node_color = values, node_size=30, with_labels=True)
-# plt.show()
-#
+if __name__ == "__main__":
+    enD = get_english_digraph()
+    nlD = get_dutch_digraph()
+
+    test = ["skirt", "potato"]
+    test_network(enD, test, 4)
+
+    #test = ["oneindig", "eeuwig"]
+    #test_network(nlD, test, 4)
+
+
