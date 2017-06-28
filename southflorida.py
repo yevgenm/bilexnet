88c40cf6da391c86c4d190d274a72f1c48a7e6b2
amir ardalan  kalantari dehgahi <amirardalankalantaridehgahi@amirs-MacBook-Pro.local>
2017-06-28 15:43:07 -0400

    added southflorida code

---
+++ southflorida.py
@@ -0,0 +1,51 @@
+from igraph import *
+import csv
+import os
+from collections import Counter
+import utils
+import pandas
+import ml_metrics as metrics
+import random
+from scipy.stats import ttest_rel
+import numpy as np
+from pajek_converter import convert_pajek
+import pickle
+import itertools
+import copy
+import pickle
+import sys
+
+
+
+FILES = ["Cue_Target_Pairs.txt", "Cue_Target_Pairs1.txt", "Cue_Target_Pairs2.txt", "Cue_Target_Pairs3.txt", "Cue_Target_Pairs4.txt" , "Cue_Target_Pairs5.txt", "Cue_Target_Pairs6.txt","Cue_Target_Pairs7.txt"]
+
+DICT = dict()
+with open('southflor.pickle', 'wb') as handle:
+    
+    
+    count = 0
+    for fil in FILES:
+        print(fil)
+        f = open(fil, "r",encoding="latin-1")
+        l = f.readlines()
+        l = l[4:-3]
+        print(len(l))
+        for line in l:
+            line = line
+            line = line.split(",")
+            cue = line[0].lower()
+            target = line[1].lower()
+            average = float(line[5])
+            
+            #print(cue)
+            count = count+1
+            if cue not in DICT:
+                DICT[cue] = dict()
+                
+            if target not in DICT[cue]:
+                DICT[cue][target] = average
+                
+    print(count)
+    pickle.dump(DICT, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        
+        
